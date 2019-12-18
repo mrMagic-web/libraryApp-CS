5ba480ca45680c934733f2c9e0f3c982da2b28a2
CSComarch <CSComarch@DESKTOP-9VN3LFO>
2019-12-18 14:40:10 +0100

    Tymczasowe zmiany

---
+++ Bibliotekarz/ViewModel/BookViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Bibliotekarz.Model;
+using Bibliotekarz.Views;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Bibliotekarz.ViewModel
+{
+    public class BookViewModel : ViewModelBase
+    {
+        internal BookWindow Window;
+
+        public BookViewModel()
+        {
+            MyBook = new Book()
+            {
+                Borrower = new Customer()
+            };
+        }
+        public Book MyBook { get; set; }
+        public ICommand SaveCommand => new RelayCommand(Save);
+        private void Save()
+        {
+            if (!MyBook.IsBorrowed)
+            {
+                MyBook.Borrower = null;
+            }
+            Window.OnBookCreated(MyBook);
+            Window.Close();
+        }
+    }
+}
---
+++ Bibliotekarz/Views/BookWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Bibliotekarz.Model;
+using Bibliotekarz.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bibliotekarz.Views
+{
+    public partial class BookWindow : Window
+    {
+        public BookWindow()
+        {
+            InitializeComponent();
+            ((BookViewModel)DataContext).Window = this;
+
+        }
+
+        public event Action<Book> BookCreated;
+
+        internal void OnBookCreated(Book myBook)
+        {
+            BookCreated?.Invoke(myBook);
+        }
+    }
+}
